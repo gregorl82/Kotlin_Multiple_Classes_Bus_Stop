31aea2097a2a7ab192ca8e8f4c050dbb5f0665ed
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-03-04 18:11:00 +0000

    Tests getters and setters for Bus class

--- src/test/java/BusTest.kt
+++ src/test/java/BusTest.kt
@@ -4,15 +4,21 @@ import org.junit.jupiter.api.Assertions.*
 
 internal class BusTest {
 
+    val bus = Bus("Buchanan Bus Station", 10)
+
     @Test
     fun getDestination() {
+        assertEquals("Buchanan Bus Station", bus.destination)
     }
 
     @Test
     fun setDestination() {
+        bus.destination = "Falkirk"
+        assertEquals("Falkirk", bus.destination)
     }
 
     @Test
     fun getCapacity() {
+        assertEquals(10, bus.capacity)
     }
 }
