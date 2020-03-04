66af4bc465fbdaf6f49a06a65269c8a32b691a70
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-03-04 18:05:04 +0000

    Initial commit

---
+++ src/main/java/Bus.kt
@@ -0,0 +1,6 @@
+class Bus (destination: String, capacity: Int) {
+
+    var destination: String = destination
+    val capacity: Int = capacity
+
+}
---
+++ src/test/java/BusTest.kt
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class BusTest {
+
+    @Test
+    fun getDestination() {
+    }
+
+    @Test
+    fun setDestination() {
+    }
+
+    @Test
+    fun getCapacity() {
+    }
+}
